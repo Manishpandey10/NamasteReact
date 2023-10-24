a1cb63b9b0d931ac17dbdd3e0626b6c589785732
Manishpandey10 <manish2310pandey@gmail.com>
2023-10-24 18:32:29 +0530

    Ep02-ignitionf our app | Manish (1832 hrs 24oct)

---
+++ ep02/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
+const parent = React.createElement("div",{id:"parent "}, 
+
+[    React.createElement("div",{id:"child",},
+    [
+        React.createElement('h1',{},"Manish this side !! 🔥"),
+        React.createElement('h2',{},"this is the nested h2 in child 1")
+    ]
+)],// here we use array so that we can create more than one childern to the div , and we can do as many as we can 
+[
+    React.createElement("div",{id:"child2",},
+    [
+        React.createElement('h3',{},"this is the nested h3 in child 2"),
+        React.createElement('h4',{},"this is the nested h4 in child 2")
+    ])
+])
+
+
+const root = ReactDOM.createRoot(document.querySelector('root')) 
+root.render(parent);
--- ep02/readme.md
+++
@@ -1,3 +0,0 @@
-1-What is NPM?
-Ans - stands for Node package manager , but it's not that.It has no full form, it manages packages behind the scene 
-

a80144535f6cdd0fb3e27df1cfd09c6357530106
Manishpandey10 <manish2310pandey@gmail.com>
2023-10-24 15:00:38 +0530

    Assignment01-NamasteReact

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* 
 <div>
-<DIV>
+<DIV> this kind of dom can be obtained by using method written below
 </div>
 *///creating multiple divs 
 const parent = React.createElement("div",{id:"parent "}, 
@@ -10,7 +10,7 @@ const parent = React.createElement("div",{id:"parent "},
         React.createElement('h1',{},"this is the nested h1"),
         React.createElement('h2',{},"this is the nested h2")
     ]
-)],
+)],// here we use array so that we can create more than one childern to the div , and we can do as many as we can 
 [
     React.createElement("div",{id:"child2",},
     [
@@ -28,3 +28,13 @@ root.render(parent);//object to html tag conversion her
 // console.log(heading);// returns object
 // root.render(heading)
 // only one render will be happen, here heading will be rendered 
+
+
+/* ABOUt ARROW FUNCTIONS*
+arraow functions are compact version of traditional fucntion , 
+where we write functions as function(parameters){}
+we can simply do this, 
+(params)=>{arguments}
+unlike functions arrow functions do not have it's this keyword, we can use this keyword but that this will be lexically bound
+i.e this will take context of the lexical scope in which the function is defined , it will not take the context of the function ()=>{} arrow fn do not have its own (this). 
+*/

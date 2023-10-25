29aa9d5a1d2285626e51012e55011c52daa21f24
Manishpandey10 <manish2310pandey@gmail.com>
2023-10-25 22:48:24 +0530

    ep3- LAYING fOUNDATION

--- ep03/app.js
+++ ep03/app.js
@@ -1,18 +1,49 @@
-const parent = React.createElement("div",{id:"parent "}, 
+import React from "react";
+import ReactDOM  from "react-dom/client";
+// react element 
+/* const heading = React.createElement("h1",{id='heading',"this is old way to add DomElement"}
+) 
+const root = ReactDom.createElement(document.getElementById('root))
+root.render(heading)*/
 
-[    React.createElement("div",{id:"child",},
-    [
-        React.createElement('h1',{},"this is the nested h1"),
-        React.createElement('h2',{},"this is the nested h2")
-    ]
-)],// here we use array so that we can create more than one childern to the div , and we can do as many as we can 
-[
-    React.createElement("div",{id:"child2",},
-    [
-        React.createElement('h3',{},"this is the nested h3"),
-        React.createElement('h4',{},"this is the nested h4")
-    ])
-])
+// using JSX (this code will be transpiled by bundler to make it understandable for browser) code-> Parcel -> Babel , babel transpiles jsx code 
+// babel converts JSX => React.CreateElement =>ReactElement.js object => HTMLELEMENT (render)
 
-const root = ReactDOM.createRoot(document.querySelector('root')) 
-root.render(parent);
+//  const JsxHeading = <h1 id="heading">Hola Amigo !!</h1> 
+// //  console.log(JsxHeading); this returns a object
+//  //JS engine won't be able to compute this , bcz this is not a correct syntax 
+
+//  const root = ReactDOM.createRoot(document.querySelector("root"))
+//  root.render(JsxHeading )
+
+//React component below
+
+//Functional component
+// const Title =()=>{
+//     return <h1 className="heading">MAnish this Side 🔥</h1>
+// }
+
+// const title2 = ()=>{
+//     return <h1 className="Head"> this is a simple react element </h1>
+
+// }
+
+// const number = 1000
+
+// //component composition , component inside of component 
+// const Heading =()=>{
+//     return <>
+//     the Number divided by 2 is {number/2} 
+//     <br/>
+//     original number :{number}
+//     <div>
+//     {title2()}
+//     </div>
+//     <Title/>
+//     <h1 >Functional component</h1>
+//     </>
+// }
+// const root = ReactDOM.createRoot(document.querySelector("root"))
+// root.render(<Heading />)
+
+// ******************aBOVE IS CLASS CODE *************************************

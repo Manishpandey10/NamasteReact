8593c413a8d1fdc4a4ad870dc258adbef14da764
Manishpandey10 <manish2310pandey@gmail.com>
2023-10-27 14:14:53 +0530

    ..

--- ep01/app.js
+++ ep01/app.js
@@ -3,29 +3,29 @@
 <DIV> this kind of dom can be obtained by using method written below
 </div>
 *///creating multiple divs 
-const parent = React.createElement("div",{id:"parent "}, 
+// const parent = React.createElement("div",{id:"parent "}, 
 
-[    React.createElement("div",{id:"child",},
-    [
-        React.createElement('h1',{},"this is the nested h1"),
-        React.createElement('h2',{},"this is the nested h2")
-    ]
-)],// here we use array so that we can create more than one childern to the div , and we can do as many as we can 
-[
-    React.createElement("div",{id:"child2",},
-    [
-        React.createElement('h3',{},"this is the nested h3"),
-        React.createElement('h4',{},"this is the nested h4")
-    ])
-])
+// [    React.createElement("div",{id:"child",},
+//     [
+//         React.createElement('h1',{},"this is the nested h1"),
+//         React.createElement('h2',{},"this is the nested h2")
+//     ]
+// )],// here we use array so that we can create more than one childern to the div , and we can do as many as we can 
+// [
+//     React.createElement("div",{id:"child2",},
+//     [
+//         React.createElement('h3',{},"this is the nested h3"),
+//         React.createElement('h4',{},"this is the nested h4")
+//     ])
+// ])
 
-const root = ReactDOM.createRoot(document.querySelector('root')) 
-root.render(parent);//object to html tag conversion her 
+// const root = ReactDOM.createRoot(document.querySelector('root')) 
+// root.render(parent);//object to html tag conversion her 
 
 
-// const heading = React.createElement("h1",{id:"heading"},"hello there!! using React !!");//single element creation using core react 
-// console.log(heading);// returns object
-// root.render(heading)
+const heading = React.createElement("h1",{id:"heading"},"hello there!! using React !!");//single element creation using core react 
+console.log(heading);// returns object
+root.render(heading)
 // only one render will be happen, here heading will be rendered 
 
 
